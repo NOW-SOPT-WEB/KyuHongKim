b6324d0df8e327e46c3941f9560078bb05584b2d
kimkyuhong <bobo3766@naver.com>
2024-04-09 12:49:42 +0900

    좌측 nav 버튼 클릭시 해당 쇼핑리스트 보여주기 기능 구현

--- shopping/index.js
+++ shopping/index.js
@@ -1,5 +1,6 @@
-const menu_hide_btn = document.querySelector(".menu_hide_btn");
+/** menu_bar show & hidden 구현 */
 const menu_bar = document.querySelector("#menu_bar");
+const menu_hide_btn = document.querySelector(".menu_hide_btn");
 const hambuger_btn = document.querySelector(".hambuger_btn");
 
 function menu_hide_handler() {
@@ -12,4 +13,25 @@ function menu_show_handler() {
 
 menu_hide_btn.addEventListener("click", menu_hide_handler);
 hambuger_btn.addEventListener("click", menu_show_handler);
-console.log(menu_remove_btn);
+/** */
+
+/** nav 클릭시 해당 쇼핑리스트 보여주기 구현 */
+const shoppingList = document.querySelectorAll(".shoppingList");
+const btn_list = document.querySelectorAll(".nav_btn");
+
+// name에 해당하는 쇼핑리스트의 display값을 block으로,
+// 나머지 쇼핑리스트들은 none값을 줘서 해당하는 쇼핑리스트만 보여주는 함수
+function showList(name) {
+    shoppingList.forEach((elem) => {
+        if (elem.id === name) elem.style.display = "block";
+        else elem.style.display = "none";
+    });
+}
+
+// nav의 각 버튼에 click 이벤트 추가
+btn_list.forEach((elem) => {
+    elem.addEventListener("click", () => {
+        showList(`${elem.className.split("_")[0]}List`);
+    });
+});
+/** */
