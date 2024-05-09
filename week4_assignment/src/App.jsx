916f4fc6bfebf50a706342c6d4e9aa16ad949ac6
kimkyuhong <bobo3766@naver.com>
2024-05-10 02:58:27 +0900

    init: 초기설정 추가

--- week4_assignment/src/App.jsx
+++ week4_assignment/src/App.jsx
@@ -1,7 +1,14 @@
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './styles/theme';
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ week4_assignment/src/styles/theme.js
@@ -0,0 +1,24 @@
+const colors = {
+  white: '#FFFFFF',
+  black: '#000000',
+  skyBlue: '#C7F5FE',
+  lightPink: '#FCC8F8',
+  lightPurple: '#EAB4F8',
+  purple: '#9900F0',
+  lightYellow: '#F3F798',
+};
+
+const fonts = {
+  xl: '2rem',
+  lg: '1.5rem',
+  md: '1.2rem',
+  sm: '1rem',
+};
+//보통은 font 객체를 만들어서 적용한다!
+
+const theme = {
+  colors,
+  fonts,
+};
+
+export default theme;
